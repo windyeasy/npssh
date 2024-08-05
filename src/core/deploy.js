8d6bffc03b418de95b121c63aef92fe624b56d7c
windyeasy <windyeasy13gb@163.com>
2024-08-05 14:31:37 +0800

    feat: add isDeleteRemoteFiles

--- src/core/deploy.js
+++ src/core/deploy.js
@@ -2,12 +2,39 @@ import path from "path";
 import { resolveAbsolutePath } from "../utils/path-utils.js";
 import { sshConnect } from "../utils/ssh-connect.js";
 
+export async function deleteDirectoryBySsh(ssh, path){
+  try {
+    const command = `rm -rf ${path}`
+    const result = await ssh.execCommand(command);
+
+    if (result.code === 0) {
+      console.log('Directory deleted successfully.');
+    } else {
+      console.error('Error deleting directory:', result.stderr);
+    }
+  } catch (error) {
+    console.error('Error connecting to server or executing command:', error);
+  }
+}
+
+/**
+ * Deploy files to remote server
+ * @param {object} config 
+ * @param {string} config.from 
+ * @param {string} config.to 
+ * @param {boolean} config.isDeleteRemoteFiles 
+ * @returns 
+ */
 export async function deploy(config){
-  const {from, to, ...options} = config
+  const {from, to, isDeleteRemoteFiles = false, ...options} = config
+
   const currentPath = resolveAbsolutePath(from)
   const ssh = await sshConnect(options)
   let errorCount = 0
   try {
+    if(isDeleteRemoteFiles){
+      await deleteDirectoryBySsh(ssh, to)
+    }
     await ssh.putDirectory(currentPath, to, {
       recursive: true,
       concurrency: 10,
--- src/utils/parse-config.js
+++ src/utils/parse-config.js
@@ -6,9 +6,8 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 export function parseConfig(configPrefix = '.npssh') {
   return new Promise((resolve, reject) => {
-    // 获取当前工作目录
+  
     const workingDirectory = process.cwd();
-    // 读取工作目录下的所有文件
     fs.readdir(workingDirectory, (err, files) => {
       if (err) {
         console.error("Error reading directory:", err);
@@ -17,21 +16,15 @@ export function parseConfig(configPrefix = '.npssh') {
       }
 
       files.forEach(async (file) => {
-        // 获取文件的完整路径
         const filePath = path.join(workingDirectory, file);
-
-        // 检查文件是否以指定的前缀开始
         if (file.startsWith(configPrefix)) {
-          // 获取文件类型
           const extension = path.extname(file);
 
           if (extension === ".cjs") {
-            // 解析 .js 文件
             const jsConfig =  require(filePath);
             resolve(jsConfig)
             
           } else if (extension === ".yml") {
-            // 解析 .yml 文件
             fs.readFile(filePath, "utf8", (err, content) => {
               if (err) {
                 console.error("Error reading .yml file:", err);
@@ -42,7 +35,6 @@ export function parseConfig(configPrefix = '.npssh') {
               resolve(ymlConfig);
             });
           } else if (extension === ".json") {
-            // 解析 .json 文件
             fs.readFile(filePath, "utf8", (err, content) => {
               if (err) {
                 console.error("Error reading .json file:", err);
